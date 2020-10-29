353778f41d97f175f6950a3753902d2ad0a4867f
denn4617 <56546859+denn4617@users.noreply.github.com>
2020-10-29 02:49:51 +0100

    Add files via upload

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+	// if (typeof weekday[FirstDay.getDay()] != 'undefined') {     // CHECK FOR 'undefined'.
+	// 	document.getElementById('fday').innerHTML = weekday[FirstDay.getDay()] +
+	// 		' (' + FirstDay.toDateString('dd/mon/yyyy') + ')';
+	// 	document.getElementById('lday').innerHTML = weekday[LastDay.getDay()] +
+	// 		' (' + LastDay.toDateString('dd/mon/yyyy') + ')'; ;
+	// }
+	// else {
+	// 	document.getElementById('fday').innerHTML = '';
+	// 	document.getElementById('lday').innerHTML = '';
+	// }
